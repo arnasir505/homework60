e5477c1be32a8547c6fa8041065c0d61ad2c8e48
Ariet Nasirdinov <ariet.nasirdinov505@gmail.com>
2024-01-28 17:52:08 +0600

    Created file with types, display messages to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
 import { useEffect, useState } from 'react';
 import { url } from './constants';
+import { Message } from './types';
 
 function App() {
-  const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState<Message[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch(url);
       const messagesData = await response.json();
-      console.log(messagesData);
+      setMessages(messagesData);
     };
     fetchData();
   }, []);
 
-  return <></>;
+  return (
+    <div className='container'>
+      {messages.map((msg) => (
+        <div key={msg._id}>{msg.message}</div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
